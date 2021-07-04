b2bc95e581ce199d137f6cb8d0c4f116380b2a9e
gaccad <86046635+gaccad@users.noreply.github.com>
2021-07-04 15:18:27 +0400

    React with Cognito Auth
    Added Cognito Auth and changes to React framework

---
+++ Website/public/get.js
@@ -0,0 +1,55 @@
+var callAPI = (CustomerName,
+			   Salesperson,
+			   Date,
+			   MeetingType,
+			   MeetingSubject,
+			   MeetingDetails,
+			   VIPCustomer,
+			   Feedback) => {
+
+	var myHeaders = new Headers();
+
+	myHeaders.append("Content-Type", "application/json");
+
+
+/* 	CustomerName = 'TEST';
+	Salesperson = 'TEST';
+	Date = '21/06/21';
+	MeetingType = 'MEETING';
+	MeetingSubject = 'Chef';
+	MeetingDetails = 'TEST';
+	VIPCustomer = 'NO';
+	Feedback = 'GOOD'; */
+	
+	// Change date format to dd/mm/yy
+	dt = Date.substring(8, 10);
+	mn = Date.substring(5, 7);
+	yy = Date.substring(2, 4);
+	
+	newdate = dt + "/" + mn + "/" + yy
+	
+
+	var raw = JSON.stringify({
+		"CustomerName": CustomerName,
+		"Salesperson": Salesperson,
+		"Date": newdate,
+		"MeetingType": MeetingType,
+		"MeetingSubject": MeetingSubject,
+		"MeetingDetails": MeetingDetails,
+		"VIPCustomer": VIPCustomer,
+		"Feedback": Feedback
+	});
+
+	var requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+
+	// make API call to pass visit data
+	fetch("https://748nxq4fc1.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+		.then(response => response.text())
+		.then(result => alert(JSON.parse(result).body))
+		.catch(error => console.log('error', error));
+}
